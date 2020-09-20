7d401c348b90e88e33945a8f44f8d6761fe2669b
Adarsh Kumar <70680860+Adarsh4062@users.noreply.github.com>
2020-09-20 14:24:52 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+    <title>Credit Management</title>
+   
+    <link rel="stylesheet" href="style.css">
+</head>
+<body >
+<div id="header">
+       <br>
+       <h1 style=" font-family:Georgia; font-size: 60px  ; "> Credit management Project </h1>
+       <br>
+        </div>
+        <div id="section">
+            <table>
+                <tr></tr>
+                <tr>        
+               
+                <div class="css-button" >
+                  <p class="css-button-text">View Users</p>
+                  <div class="css-button-inner">
+                  <a href="getdetail.php" >
+                  <div class="reset-skew">
+                  <p class="css-button-inner-text">View Users</p>
+                </div></a>
+                </div>
+                </div>
+
+                </td>
+                </tr>
+
+                <tr>        
+               <br> <br> <br> <br> <br>
+               <div class="css-button" >
+                 <p class="css-button-text">Credit Transfer</p>
+                 <div class="css-button-inner">
+                 <a href="selectuser.php" >
+                 <div class="reset-skew">
+                 <p class="css-button-inner-text">Credit Transfer</p>
+               </div></a>
+               </div>
+               </div>
+
+               </td>
+               </tr>
+
+            </table>
+    </div>
+
+</body>
+</html>
